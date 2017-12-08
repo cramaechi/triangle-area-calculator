c99958e5efc07a0ba5c67b0a6bb5f01988b81879
Chibuikem Amaechi <chibuikem_amaechi@Chibuikems-MacBook-Pro.local>
2017-12-08 11:50:08 -0600

    Modified .main.cpp.swp and main.cpp

--- main.cpp
+++ main.cpp
@@ -20,49 +20,49 @@ bool sidesOK(int s1, int s2, int s3);
 
 int main()
 {
-	int s1, s2, s3, perimeter;
-	double area;
+    int s1, s2, s3, perimeter;
+    double area;
 
-	input(s1,s2,s3);
-	areaAndPerimeter(s1,s2,s3,area,perimeter);
-	output(area,perimeter);
+    input(s1,s2,s3);
+    areaAndPerimeter(s1,s2,s3,area,perimeter);
+    output(area,perimeter);
 
-	return 0;
+    return 0;
 }
 
 void input(int& s1, int& s2, int& s3)
 {
-	cout<<"Enter three sides of an arbitrary triangle: ";
-	cin>>s1>>s2>>s3;
+    cout<<"Enter three sides of an arbitrary triangle: ";
+    cin>>s1>>s2>>s3;
 
     if (sidesOK(s1,s2,s3))
-	{
-		s1 = abs(s1);
-	    s2 = abs(s2);
-	    s3 = abs(s3);
-	}
-	else
-	{
-		cout<<"Fatal Error: Illegal input. Aborting program.";
-		exit(1);
-	}
+    {
+        s1 = abs(s1);
+        s2 = abs(s2);
+        s3 = abs(s3);
+    }
+    else
+    {
+        cout<<"Fatal Error: Illegal input. Aborting program.";
+        exit(1);
+    }
 }
 void areaAndPerimeter(int s1,int s2, int s3, double& area, int& perimeter)
 {
-	perimeter = s1 + s2 + s3;
-	double s = perimeter/2.0;
-	area = sqrt((s*(s - s1)*(s - s2)*(s - s3)));
+    perimeter = s1 + s2 + s3;
+    double s = perimeter/2.0;
+    area = sqrt((s*(s - s1)*(s - s2)*(s - s3)));
 }
 
 void output(double area, int perimeter)
 {
-	cout.setf(ios::showpoint);
-	cout.setf(ios::fixed);
-	cout.precision(1);
+    cout.setf(ios::showpoint);
+    cout.setf(ios::fixed);
+    cout.precision(1);
     if (area > 0)
     {
-	    cout<<"The area of the triangle is: "<<area<<endl;
-	    cout<<"The perimeter of the triangle is: "<<perimeter<<endl;
+        cout<<"The area of the triangle is: "<<area<<endl;
+        cout<<"The perimeter of the triangle is: "<<perimeter<<endl;
     }
     else
         cout<<"The area of the triange could not be computed."<<endl;
@@ -70,5 +70,5 @@ void output(double area, int perimeter)
 
 bool sidesOK(int s1, int s2, int s3)
 {
-	return (s1 > 0 && s2 > 0  && s3 > 0);
+    return (s1 > 0 && s2 > 0  && s3 > 0);
 }
